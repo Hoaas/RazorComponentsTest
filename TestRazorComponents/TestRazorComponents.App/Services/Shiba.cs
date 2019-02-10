9ff26d9385eda4d4248a4b4dec204655a00601f5
Terje Hoås <terjehoaas@gmail.com>
2019-02-10 17:26:24 +0100

    Trying to move the page from / to /shiba.

--- TestRazorComponents/TestRazorComponents.App/Services/Shiba.cs
+++ TestRazorComponents/TestRazorComponents.App/Services/Shiba.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TestRazorComponents.App.Services
+﻿namespace TestRazorComponents.App.Services
 {
     public class Shiba
     {
