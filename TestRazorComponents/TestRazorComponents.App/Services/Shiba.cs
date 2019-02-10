de7433d45deea8155f2bcb75f56f17aa36a5cafd
Terje Hoås <terjehoaas@gmail.com>
2019-02-10 15:37:37 +0100

    Razor Components project!

---
+++ TestRazorComponents/TestRazorComponents.App/Services/Shiba.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestRazorComponents.App.Services
+{
+    public class Shiba
+    {
+        public string Url { get; internal set; }
+    }
+}
---
+++ TestRazorComponents/TestRazorComponents.App/Services/ShibaService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestRazorComponents.App.Services
+{
+    public class ShibaService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ShibaService(IHttpClientFactory httpClientFactory) 
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<Shiba>> GetShibas()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var stuff = await client.GetAsync("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true");
+
+            var urls = await stuff.Content.ReadAsAsync<List<string>>();
+
+            return urls
+                .Select(x => new Shiba { Url = x })
+                .ToList();
+        }
+    }
+}
